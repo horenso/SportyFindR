93e79c42f8dab5f6b27547f1b0adb1434d6f97f1
Victoria Leskoschek <victoria.leskoschek@tuwien.ac.at>
2020-11-22 17:09:53 +0100

    #16 implemented location entities

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/LocationDto.java
@@ -0,0 +1,17 @@
+package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+public class LocationDto {
+
+    private Long id;
+    private double latitude;
+    private double longitude;
+
+}
---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/LocationMapper.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.LocationDto;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Location;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface LocationMapper {
+
+    Location locationDtoToLocation(LocationDto location);
+    Location locationToLocationDto(Location location);
+}
