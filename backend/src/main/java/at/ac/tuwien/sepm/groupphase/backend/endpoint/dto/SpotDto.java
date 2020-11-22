c1a3fe77114ca89fa0d019602ebbaaf77217b114
Victoria Leskoschek <victoria.leskoschek@tuwien.ac.at>
2020-11-22 18:04:33 +0100

    #17 implemented spot entities

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/SpotDto.java
@@ -0,0 +1,18 @@
+package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+public class SpotDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private LocationDto location;
+}
+
