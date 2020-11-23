0c1559b68561f96df33661bde6c016df16f989d2
Victoria Leskoschek <victoria.leskoschek@tuwien.ac.at>
2020-11-23 18:30:43 +0100

    #22 fixed issue with Location entity

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/LocationDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/LocationDto.java
@@ -14,4 +14,34 @@ public class LocationDto {
     private double latitude;
     private double longitude;
 
+    public LocationDto(){}
+
+    public LocationDto(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
