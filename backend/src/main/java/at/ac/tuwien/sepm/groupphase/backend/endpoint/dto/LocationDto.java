8e79f77b31025c425678c9b2454ad23e11361f12
Victoria Leskoschek <victoria.leskoschek@tuwien.ac.at>
2020-11-24 14:40:48 +0100

    #22 fixed bug with lombok

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/LocationDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/LocationDto.java
@@ -7,41 +7,10 @@ import lombok.*;
 @Setter
 @ToString
 @EqualsAndHashCode
-@Builder
 public class LocationDto {
 
     private Long id;
     private double latitude;
     private double longitude;
 
-    public LocationDto(){}
-
-    public LocationDto(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
 }
--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Location.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Location.java
@@ -22,35 +22,4 @@ public class Location {
     @Column(nullable = false, length = 10)
     private double longitude;
 
-    public Location(){}
-
-    public Location(Long id, double latitude, double longitude) {
-        this.id = id;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
 }
