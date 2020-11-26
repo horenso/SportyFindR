b805833e8a2eed79924a5295b2a9ca7c382f7f89
Fisnik Miftari <e11776840@student.tuwien.ac.at>
2020-11-26 05:34:57 +0100

    #18 edited create spot by separating two cases (with exist. Location and with new Location)

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/SimpleSpotService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/SimpleSpotService.java
@@ -1,6 +1,8 @@
 package at.ac.tuwien.sepm.groupphase.backend.service.impl;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Location;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Spot;
 import at.ac.tuwien.sepm.groupphase.backend.repository.SpotRepository;
+import at.ac.tuwien.sepm.groupphase.backend.service.LocationService;
 import at.ac.tuwien.sepm.groupphase.backend.service.SpotService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,14 +15,22 @@ public class SimpleSpotService implements SpotService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final SpotRepository spotRepository;
+    private final LocationService locationService;
 
-    public SimpleSpotService(SpotRepository spotRepository) {
+    public SimpleSpotService(SpotRepository spotRepository, LocationService locationService) {
         this.spotRepository = spotRepository;
+        this.locationService = locationService;
     }
 
     @Override
     public Spot create(Spot spot) {
         LOGGER.debug("Create new Spot {}", spot);
+        Location location = spot.getLocation();
+        if (location.getId()==null && location.getLatitude()!=0.0 && location.getLongitude()!=0.0) {
+            locationService.create(spot.getLocation());
+        }
         return spotRepository.save(spot);
     }
+
+
 }
