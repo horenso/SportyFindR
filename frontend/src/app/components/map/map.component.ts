ba4baf912a9b732832a5dab788e4813459193b4b
Marcel Jira (00250890) <marcel.jira@tuwien.ac.at>
2020-12-17 12:16:09 +0100

    Add a rough map of the world around Austria. Also switch to to (less detailed) png version of basemap (the jpg-version wouldn't play nice with a surrounding map). Refine some leafletOptions

--- frontend/src/app/components/map/map.component.ts
+++ frontend/src/app/components/map/map.component.ts
@@ -18,13 +18,21 @@ export class MapComponent implements OnInit, OnDestroy {
 
   private locLayerGroup: LayerGroup<Marker> = new LayerGroup<Marker>();
 
-  private basemap = 'https://maps{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpg';
+  private worldMap = 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg';
+//  private basemap = 'https://maps{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpg'; // high def but jpg, so no background layer is possible
+  private basemap = 'https://maps{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png'
 
   layers: Layer[] = [
+    tileLayer(this.worldMap, {
+      attribution: 'World Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abcd',
+      minZoom: 1,
+      maxZoom: 16,
+    }),
     tileLayer(this.basemap, {
       minZoom: 1,
       maxZoom: 20,
-      attribution: 'Data Source: <a href="https://www.basemap.at">basemap.at</a>',
+      attribution: 'Data Source Austria: <a href="https://www.basemap.at">basemap.at</a>',
       subdomains: ['', '1', '2', '3', '4'],
       bounds: [[46.35877, 8.782379], [49.037872, 17.189532]]
     })
@@ -36,7 +44,9 @@ export class MapComponent implements OnInit, OnDestroy {
     zoomDelta: 0.5,
     wheelPxPerZoomLevel: 90,
     zoomSnap: 0,
-    cursor: true
+    cursor: true,
+    minZoom: 1,
+    maxZoom: 20,
   };
 
   onMapReady(map: Map) {
