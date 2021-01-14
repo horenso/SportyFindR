0ca4347942a81c8d0e74acea55236337cf1d0300
Fisnik Miftari <e11776840@student.tuwien.ac.at>
2021-01-14 18:59:07 +0100

    #101 hashtag service doc

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/CategoryDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/CategoryDto.java
@@ -2,6 +2,8 @@ package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -12,5 +14,7 @@ import lombok.*;
 public class CategoryDto {
 
     private Long id;
+
+    @Size(min = 1, max = 32, message = "Category name should be between 1 and 32 characters long")
     private String name;
 }
