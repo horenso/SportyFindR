b13a7c86d9dd07a96d47340a5634126b6d7e5d50
Jannis Adamek <e11809490@student.tuwien.ac.at>
2021-01-15 08:59:58 +0100

    messages show more

--- frontend/src/app/components/text-with-hashtags/text-with-hashtags.component.ts
+++ frontend/src/app/components/text-with-hashtags/text-with-hashtags.component.ts
@@ -1,19 +1,31 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterContentInit, AfterViewInit, ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-text-with-hashtags [text]',
   templateUrl: './text-with-hashtags.component.html',
   styleUrls: ['./text-with-hashtags.component.scss']
 })
-export class TextWithHashtagsComponent implements OnInit {
+export class TextWithHashtagsComponent implements OnInit, AfterViewInit {
 
   @Input() text: string;
+  @Input() maxRows: number = 3;
   @Output() clickedHashtag = new EventEmitter();
 
+  @ViewChild('textArea', {static: false}) textAreaElement: ElementRef;
+
   tokens: string[] = [];
+  textTooLong: boolean = false;
+  showAll: boolean = false;
 
   ngOnInit(): void {
     this.tokens = this.text.split(/(#[A-Za-z0-9]+)/g);
+  }
+
+  ngAfterViewInit(): void {
+    const lineHeight = parseInt(window.getComputedStyle(this.textAreaElement.nativeElement).lineHeight);
+    const elementHeight = this.textAreaElement.nativeElement.scrollHeight;
+
+    setTimeout(() => this.textTooLong = (elementHeight / lineHeight) > this.maxRows);
   }
 
   public isHashtag(word: string) {
@@ -23,6 +35,14 @@ export class TextWithHashtagsComponent implements OnInit {
   public onClickedHashtag(hashtag: string): void {
     console.log('Clicked on hashtag: ' + hashtag);
     this.clickedHashtag.emit(hashtag);
+  }
+
+  public getTextAreaClass(): string {
+    if (!this.showAll) {
+      return 'lineClamp';
+    } else {
+      return '';
+    }
   }
 
   // this is to prevent the double click select on hashtags 
