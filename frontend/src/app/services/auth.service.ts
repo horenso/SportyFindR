e167a531c1d4ee29a20c43b31a5088d5ad99f832
Simon Garuda Linder <e11728330@student.tuwien.ac.at>
2021-01-30 22:23:12 +0100

    fix bug #151

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,10 +1,12 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnDestroy, OnInit} from '@angular/core';
 import {AuthRequest} from '../dtos/auth-request';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {tap} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 import jwt_decode from 'jwt-decode';
 import {Globals} from '../global/globals';
+import {User} from '../dtos/user';
+import {Reaction, ReactionType} from '../dtos/reaction';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +14,16 @@ import {Globals} from '../global/globals';
 export class AuthService {
 
   private authBaseUri: string = this.globals.backendUri + '/authentication';
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
   constructor(
     private httpClient: HttpClient,
     private globals: Globals) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.setUser();
+    this.currentUser = this.currentUserSubject.asObservable();
   }
-
   /**
    * Login in the user. If it was successful, a valid JWT token will be stored
    * @param authRequest User data
@@ -25,10 +31,26 @@ export class AuthService {
   loginUser(authRequest: AuthRequest): Observable<string> {
     return this.httpClient.post(this.authBaseUri, authRequest, {responseType: 'text'})
       .pipe(
-        tap((authResponse: string) => this.setToken(authResponse))
+        tap((authResponse: string) => {
+            this.setToken(authResponse);
+            this.currentUserSubject.next(jwt_decode(authResponse));
+          }
+        )
       );
   }
+  private setUser(): void {
+    this.currentUserSubject.next(jwt_decode(this.getToken()));
+  }
 
+  public get currentUserEmail(): String {
+    return this.currentUserSubject.value['sub'];
+  }
+  public get isUserAdmin(): Boolean {
+    if (this.currentUserSubject.value['rol'].some(x => x === 'ROLE_ADMIN')) {
+      return true;
+    }
+    return false;
+  }
 
   /**
    * Check if a valid JWT token is saved in the localStorage
@@ -77,5 +99,4 @@ export class AuthService {
     date.setUTCSeconds(decoded.exp);
     return date;
   }
-
 }
